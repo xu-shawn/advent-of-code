c1c1c8646f6b5faeb8d8c8798db9fb2bf266e956
Shawn Xu <xu107288696@gmail.com>
2024-12-02 22:57:33 -0800

    improve code

--- 2024/3/solution.cpp
+++ 2024/3/solution.cpp
@@ -15,7 +15,7 @@ void solve(Data&&);
 void solve_q1(Data&);
 void solve_q2(Data&);
 int  evaluate_string(const std::string&);
-int  evaluate_instruction(const std::sregex_iterator& instruction);
+int  evaluate_instruction(const std::smatch&);
 
 struct Data {
     std::string data;
@@ -29,15 +29,15 @@ struct Data {
 
 Data parse_from(std::fstream&& file) {
     Data               result;
-    std::ostringstream file_stream;
+    std::ostringstream read_stream;
     std::string        line;
 
-    file_stream << "do()";
+    read_stream << "do()";
     while (file >> line)
-        file_stream << line;
-    file_stream << "don't()";
+        read_stream << line;
+    read_stream << "don't()";
 
-    result.data = file_stream.str();
+    result.data = read_stream.str();
     return result;
 }
 
@@ -46,41 +46,45 @@ void solve(Data&& data) {
     solve_q2(data);
 }
 
-void solve_q1(Data& data) {
+void solve_q1(Data& data) { std::cout << evaluate_string(data.data) << std::endl; }
+
+void solve_q2(Data& data) {
     using std::cbegin;
     using std::cend;
 
-    std::cout << evaluate_string(data.data) << std::endl;
-}
+    static const std::regex enabling_regex(R"regex(do\(\).+?don't\(\))regex");
 
-void solve_q2(Data& data) {
-    int        ans = 0;
-    std::regex enabling_regex(R"regex(do\(\).+?don't\(\))regex");
+    int ans = 0;
 
-    auto match_begin     = std::sregex_iterator(cbegin(data.data), cend(data.data), enabling_regex);
+    const auto match_begin =
+      std::sregex_iterator(cbegin(data.data), cend(data.data), enabling_regex);
     const auto match_end = std::sregex_iterator();
 
-    for (std::sregex_iterator curr_match = match_begin; curr_match != match_end; ++curr_match)
+    for (auto curr_match = match_begin; curr_match != match_end; ++curr_match)
         ans += evaluate_string(curr_match->str());
 
     std::cout << ans << std::endl;
 }
 
 int evaluate_string(const std::string& data) {
+    using std::cbegin;
+    using std::cend;
+
+    static const std::regex mul_regex(R"regex(mul\((\d+),(\d+)\))regex");
+
     int ans = 0;
 
-    std::regex mul_regex(R"regex(mul\((\d+),(\d+)\))regex");
-    auto       match_begin = std::sregex_iterator(cbegin(data), cend(data), mul_regex);
+    const auto match_begin = std::sregex_iterator(cbegin(data), cend(data), mul_regex);
     const auto match_end   = std::sregex_iterator();
 
     for (std::sregex_iterator curr_match = match_begin; curr_match != match_end; ++curr_match)
-        ans += evaluate_instruction(curr_match);
+        ans += evaluate_instruction(*curr_match);
 
     return ans;
 }
 
-int evaluate_instruction(const std::sregex_iterator& instruction) {
-    return std::stoi((*instruction)[1]) * std::stoi((*instruction)[2]);
+int evaluate_instruction(const std::smatch& instruction) {
+    return std::stoi(instruction[1]) * std::stoi(instruction[2]);
 }
 
 int main() { solve(parse_from(std::fstream("data.txt"))); }
