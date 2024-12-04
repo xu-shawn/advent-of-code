79d796af2c50538c1975da346c878a9c6370ab0a
Shawn Xu <xu107288696@gmail.com>
2024-12-03 22:50:07 -0800

    fix bug

--- 2024/4/solution.cpp
+++ 2024/4/solution.cpp
@@ -15,15 +15,12 @@ Data parse_from(std::fstream&&);
 void solve(Data&&);
 void solve_q1(const Data&);
 void solve_q2(const Data&);
-int  search_all_matches(const Data&, const std::string&, const Position);
-bool cross_xmas_match(const Data&, const Position);
-bool match_success(const std::vector<std::string>&,
-                   const std::string&,
-                   const Position,
-                   const Direction);
+int  search_all_matches(const std::vector<std::string>&, const std::string&, const Position);
+bool cross_xmas_match(const std::vector<std::string>&, const Position);
+bool match_success(const std::vector<std::string>&, const std::string&, Position, const Direction);
 
-constexpr Position operator+(const Position& lhs, const std::pair<int, int> direction) noexcept;
-Position&          operator+=(Position& lhs, const std::pair<int, int> direction) noexcept;
+constexpr Position operator+(const Position, const Direction) noexcept;
+Position&          operator+=(Position&, const Direction) noexcept;
 
 struct Direction {
     const int x;
@@ -35,7 +32,7 @@ struct Direction {
 };
 
 constexpr Direction operator+(const Direction& first, const Direction& second) {
-    return Direction(first.x + second.x, first.x + second.y);
+    return Direction(first.x + second.x, first.y + second.y);
 }
 
 constexpr Direction N{1, 0};
@@ -55,11 +52,11 @@ struct Position {
     int y;
 };
 
-constexpr Position operator+(const Position& pos, const Direction& direction) noexcept {
+constexpr Position operator+(const Position pos, const Direction direction) noexcept {
     return Position{pos.x + direction.x, pos.y + direction.y};
 }
 
-Position& operator+=(Position& pos, const Direction& direction) noexcept {
+Position& operator+=(Position& pos, const Direction direction) noexcept {
     pos.x += direction.x;
     pos.y += direction.y;
     return pos;
@@ -99,7 +96,7 @@ void solve_q1(const Data& data) {
 
     for (int x = 0; x < size(data.data); x++)
         for (int y = 0; y < size(data.data[0]); y++)
-            ans += search_all_matches(data, "XMAS", Position{x, y});
+            ans += search_all_matches(data.data, "XMAS", Position{x, y});
 
     std::cout << ans << std::endl;
 }
@@ -111,56 +108,52 @@ void solve_q2(const Data& data) {
 
     for (int x = 1; x + 1 < size(data.data); x++)
         for (int y = 1; y + 1 < size(data.data[0]); y++)
-            ans += cross_xmas_match(data, Position{x, y});
+            ans += cross_xmas_match(data.data, Position{x, y});
 
     std::cout << ans << std::endl;
 }
 
-int search_all_matches(const Data& data, const std::string& to_search, const Position position) {
+int search_all_matches(const std::vector<std::string>& map,
+                       const std::string&              to_search,
+                       const Position                  position) {
     using std::size;
-
-    const std::vector<std::string>& map = data.data;
 
     int ans = 0;
 
-    for (const auto curr_direction : ALL_DIRECTIONS)
+    for (const Direction curr_direction : ALL_DIRECTIONS)
         ans += match_success(map, to_search, position, curr_direction);
 
     return ans;
 }
 
-bool cross_xmas_match(const Data& data, const Position position) {
-    const auto [x, y]                   = position;
-    const std::vector<std::string>& map = data.data;
+bool cross_xmas_match(const std::vector<std::string>& map, const Position position) {
+    const auto [x, y] = position;
 
     if (x < 1 || y < 1 || x + 1 >= size(map) || y + 1 >= size(map.at(0)))
         return false;
 
-    return (match_success(data.data, "MAS", Position{x + 1, y + 1}, SW)
-            || match_success(data.data, "SAM", Position{x + 1, y + 1}, SW))
-        && (match_success(data.data, "MAS", Position{x - 1, y + 1}, NW)
-            || match_success(data.data, "SAM", Position{x - 1, y + 1}, NW));
+    return (match_success(map, "MAS", Position{x + 1, y + 1}, SW)
+            || match_success(map, "SAM", Position{x + 1, y + 1}, SW))
+        && (match_success(map, "MAS", Position{x - 1, y + 1}, NW)
+            || match_success(map, "SAM", Position{x - 1, y + 1}, NW));
 }
 
 bool match_success(const std::vector<std::string>& map,
                    const std::string&              to_search,
-                   const Position                  position,
+                   Position                        position,
                    const Direction                 direction) {
     using std::size;
 
-    auto [x, y]                           = position;
-    const auto [x_direction, y_direction] = direction;
-
     for (const char character : to_search)
     {
-        if (x < 0 || y < 0 || x >= size(map) || y >= size(map.at(0)))
+        if (position.x < 0 || position.y < 0 || position.x >= size(map)
+            || position.y >= size(map.at(0)))
             return false;
 
-        if (map[x][y] != character)
+        if (map[position.x][position.y] != character)
             return false;
 
-        x += x_direction;
-        y += y_direction;
+        position += direction;
     }
 
     return true;
