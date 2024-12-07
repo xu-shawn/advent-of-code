b2404600467a2cc002d5ce234170a7b539dc8bef
Shawn Xu <xu107288696@gmail.com>
2024-12-06 22:59:58 -0800

    speedup

--- 2024/7/solution.cpp
+++ 2024/7/solution.cpp
@@ -18,29 +18,34 @@ template<typename Iterator, typename T, typename... Ops>
 bool is_possible(const T&, Iterator, Iterator, Ops...);
 template<typename Iterator, typename T, typename... Ops>
 bool search_for_possible(const T&, const T&, const Iterator, const Iterator, Ops...);
+template<typename T>
+constexpr T power(const T base, const std::int32_t exponent);
+
+template<typename T>
+constexpr T power(const T base, const std::int32_t exponent) {
+    if (exponent == 0)
+        return static_cast<T>(1);
+
+    if (exponent == 1)
+        return base;
+
+    return power(base, exponent / 2) * power(base, exponent / 2) * power(base, exponent % 2);
+}
 
 struct Concatenate {
-    std::uint64_t operator()(uint64_t a, uint64_t b) const {
-        std::string s = std::to_string(a) + std::to_string(b);
-        return std::stoull(s);
+    std::uint64_t operator()(const std::uint64_t a, const std::uint64_t b) const {
+        std::uint64_t b_cpy = b;
+        int           b_len = 0;
+
+        while (b_cpy)
+        {
+            b_cpy /= 10;
+            b_len++;
+        }
+
+        return a * power(10, b_len) + b;
     }
 };
-
-std::vector<std::string> split(const std::string& str, char delim) {
-    std::vector<std::string> result{};
-
-    std::istringstream stream{str};
-
-    for (std::string token{}; std::getline(stream, token, delim);)
-    {
-        if (token.empty())
-            continue;
-
-        result.push_back(token);
-    }
-
-    return result;
-}
 
 struct Data {
     std::vector<std::pair<std::uint64_t, std::vector<std::uint64_t>>> data;
@@ -53,19 +58,35 @@ struct Data {
 };
 
 Data parse_from(std::fstream&& file) {
+    using std::string;
+
     Data        result;
     std::string line;
 
     while (std::getline(file, line))
     {
-        std::vector<std::string>   tokens = split(line, ' ');
         std::vector<std::uint64_t> resources;
+        std::size_t                pos = 0;
 
-        tokens[0].pop_back();
-        const std::int64_t target = std::stoull(tokens[0]);
+        const std::uint64_t target = std::stoull(line, &pos);
+        std::uint64_t       temp   = 0;
 
-        for (int i = 1; i < size(tokens); i++)
-            resources.push_back(std::stoull(tokens[i]));
+        for (size_t i = pos + 2; i < size(line); i++)
+        {
+            if (!std::isdigit(line[i]))
+            {
+                resources.push_back(temp);
+                temp = 0;
+            }
+
+            else
+            {
+                temp *= 10;
+                temp += line[i] - '0';
+            }
+        }
+
+        resources.push_back(temp);
 
         result.data.emplace_back(target, std::move(resources));
     }
@@ -86,7 +107,7 @@ void solve_q1(const Data& data) {
     for (const auto& row : data.data)
     {
         if (is_possible(row.first, std::cbegin(row.second), std::cend(row.second),
-                        std::plus<std::uint64_t>{}, std::multiplies<std::uint64_t>{}))
+                        std::multiplies<std::uint64_t>{}, std::plus<std::uint64_t>{}))
             ans += row.first;
     }
 
@@ -101,8 +122,8 @@ void solve_q2(const Data& data) {
     for (const auto& row : data.data)
     {
         if (is_possible(row.first, std::cbegin(row.second), std::cend(row.second),
-                        std::plus<std::uint64_t>{}, std::multiplies<std::uint64_t>{},
-                        Concatenate{}))
+                        std::multiplies<std::uint64_t>{}, Concatenate{},
+                        std::plus<std::uint64_t>()))
             ans += row.first;
     }
 
